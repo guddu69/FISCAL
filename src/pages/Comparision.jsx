b36a6fcb217deb4554483997b849e63b5ff6c3c9
guddu69 <gurkamalbrar693@gmail.com>
2024-07-31 04:12:15 +0530

    Comparision.jsx

--- src/pages/Comparision.jsx
+++ src/pages/Comparision.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import "./family.css";
 import { useFamilyContext } from './FamilyContext';
@@ -21,39 +20,10 @@ const Comparision = () => {
       ...prevState,
       [invested_year]: value,
     }));
-=======
-// RelianceData.js
-import React, { useState, useEffect } from 'react';
-import Papa from 'papaparse';
-// import './RelianceData.css';
-
-const Comparision = () => {
-  const [year, setYear] = useState('');
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [company, setCompany] = useState('Reliance');
-
-  useEffect(() => {
-    fetch('/RELIANCE.csv')
-      .then((response) => response.text())
-      .then((text) => {
-        Papa.parse(text, {
-          header: true,
-          complete: (results) => {
-            setData(results.data);
-          },
-        });
-      });
-  }, []);
-
-  const handleChange = (e) => {
-    setYear(e.target.value);
->>>>>>> f91e3110de274a4c92195e4d080758afa38eee32
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-<<<<<<< HEAD
     // addFamilyMember(familyMember);
     setFamilyMembers([...familyMembers, familyMember]);
     setFamilyMember({
@@ -107,37 +77,6 @@ const Comparision = () => {
             <button onClick={() => handleDelete(index)}>Delete</button>
           </div>
         ))}
-=======
-    const filtered = data.filter((row) => row.Date.startsWith(year));
-    setFilteredData(filtered);
-  };
-
-  const renderClosePrices = () => {
-    if (filteredData.length > 0) {
-      return filteredData.map((row, index) => (
-        <div key={index}>
-          <p>{`Date: ${row.Date}, Close Price: ${row['Close']}`}</p>
-        </div>
-      ));
-    } else {
-      return <p>No data available for the year {year}</p>;
-    }
-  };
-
-  return (
-    <div className="reliance-container">
-      <h2>Reliance Stock Data</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Enter Year:</label>
-          <input type="number" value={year} onChange={handleChange} />
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      <div>
-        <h3>{company} Close Prices for {year}</h3>
-        {year && renderClosePrices()}
->>>>>>> f91e3110de274a4c92195e4d080758afa38eee32
       </div>
     </div>
   );
